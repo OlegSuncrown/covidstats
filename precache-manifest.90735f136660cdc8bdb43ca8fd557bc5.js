0ae10ddd753fda520b2bcc2b5ea697f94f7813df
OlegSuncrown <olegsuncrown@gmail.com>
2020-05-25 12:19:14 +0300

    Updates

---
+++ precache-manifest.90735f136660cdc8bdb43ca8fd557bc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938d5d37f9cf4ed94e5b19807170f861",
+    "url": "/covidstats/index.html"
+  },
+  {
+    "revision": "48cf6af429481a3112f5",
+    "url": "/covidstats/static/css/main.4f895fd1.chunk.css"
+  },
+  {
+    "revision": "540911aadb1b5ce2f1fb",
+    "url": "/covidstats/static/js/2.22d7069d.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/covidstats/static/js/2.22d7069d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48cf6af429481a3112f5",
+    "url": "/covidstats/static/js/main.086ad5ca.chunk.js"
+  },
+  {
+    "revision": "aed45085a6e818ed1531",
+    "url": "/covidstats/static/js/runtime-main.55fed1f0.js"
+  }
+]);
